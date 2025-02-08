13fbc62f4346971981c08c508a452ae9f22e64ea
Aie Zaldy Biocarles Quinto <aibi.quinto.up@phinmaed.com>
2025-02-08 23:59:02 +0800

    INIT project UPang-Registrar

---
+++ backend-server/src/main/kotlin/Application.kt
@@ -0,0 +1,15 @@
+package com.ite393group5
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureMonitoring()
+    configureSerialization()
+    configureDatabases()
+    configureRouting()
+}
